536f4b217b27c5bd03220028a448aabce683b55d
Gabe Giosia <gabe@ghearing.com>
2019-04-26 10:24:26 -0700

    remove version_string

--- src/scripts/check_reqs.js
+++ src/scripts/check_reqs.js
@@ -7,12 +7,7 @@ var child_process = require('child_process');
 var XCODEBUILD_NOT_FOUND_MESSAGE = 'Please install Xcode from the Mac App Store.';
 var TOOL = 'xcodebuild';
 
-var xcode_version = child_process.spawn(TOOL, ['-version']),
-	version_string = '';
-
-xcode_version.stdout.on('data', function (data) {
-	version_string += data;
-});
+var xcode_version = child_process.spawn(TOOL, ['-version']);
 
 xcode_version.stderr.on('data', function (data) {
 	console.log('stderr: ' + data);
